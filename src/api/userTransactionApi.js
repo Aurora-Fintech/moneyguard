f828bf077b0b10bf3da79a31c56f77b8678b1a50
Cemrdeniz <blackviens@outlook.com>
2025-10-19 00:56:43 +0300

    feat: add removeToken function to clear Authorization header

--- src/api/userTransactionApi.js
+++ src/api/userTransactionApi.js
@@ -7,3 +7,6 @@ export const userTransactionsApi = axios.create({
 export const setToken = (token) => {
   userTransactionsApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
+export const removeToken = () => {
+  userTransactionsApi.defaults.headers.common.Authorization = ``;
+};
