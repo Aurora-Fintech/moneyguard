9e3bc7ec3c708ebe4ca47be8b5511c6b18c6a695
begumnarmanli <begumnarmanli@gmail.com>
2025-10-21 12:14:52 +0300

    feat: statistic layout

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,26 +1,28 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import styles from "./Balance.module.css";
+import React from "react";
+import { useSelector } from "react-redux";
+import styles from "./Balance.module.css";
 
 const selectTotalBalance = (state) => state.transactions?.balance;
 
-// const Balance = () => {
-//   const totalBalance = useSelector(selectTotalBalance);
+// para birimi için
+const formatBalance = (amount) => {
+  const validAmount = Number.isFinite(amount) ? amount : 0;
+  return new Intl.NumberFormat("tr-TR", {
+    style: "currency",
+    currency: "TRY",
+    minimumFractionDigits: 2,
+  }).format(validAmount);
+};
 
-//   const formatBalance = (amount) => {
-//     return new Intl.NumberFormat("tr-TR", {
-//       style: "currency",
-//       currency: "TRY",
-//       minimumFractionDigits: 2,
-//     }).format(amount);
-//   };
+const Balance = () => {
+  const totalBalance = useSelector(selectTotalBalance);
 
-//   return (
-//     <div className={styles.balanceContainer}>
-//       <p className="dashboard-balance-txt">YOUR BALANCE</p>
-//       <div className="dashboard-value">{formatBalance(totalBalance)}</div>
-//     </div>
-//   );
-// };
+  return (
+    <div className={styles.balanceContainer}>
+      <p className={styles.dashboardBalanceTxt}>YOUR BALANCE</p>
+      <div className="dashboard-value">{formatBalance(totalBalance)}</div>
+    </div>
+  );
+};
 
-// export default Balance;
+export default Balance;
