bd0b6b50b98e3acd6563a0610026eed3327ae8d0
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-10-25 14:57:12 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./app/store";
 import AppRouter from "./routes/AppRouter";
-import { BrowserRouter } from "react-router-dom";
 import 'normalize.css';
 import "./styles/global.css";
 
@@ -12,9 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
-          <AppRouter />
-        </BrowserRouter>
+        <AppRouter />
       </PersistGate>
     </Provider>
   </React.StrictMode>
